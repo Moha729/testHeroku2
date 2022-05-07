59322d4de6f1c4bf171d69e838ad0e9f2bf43a5b
moha <moha729g@stud.kea.dk>
2022-05-07 20:42:12 +0200

    new

---
+++ src/test/java/com/example/testheroku2/TestHeroku2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testheroku2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHeroku2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
