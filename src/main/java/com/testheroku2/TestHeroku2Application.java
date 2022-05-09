b82f45f9c987c0c5d6cd187f50b451d54318ed61
moha <moha729g@stud.kea.dk>
2022-05-09 23:18:10 +0200

    new4

---
+++ src/main/java/com/testheroku2/TestHeroku2Application.java
@@ -0,0 +1,27 @@
+package com.example.testheroku2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class TestHeroku2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestHeroku2Application.class, args);
+    }
+
+}
+/*
+@RestController
+class HomeController{
+
+    @GetMapping("/")
+    String hello(){
+        return "Hey bro.\nHvis du kan se den " +
+                "meddelese, så betyder det, " +
+                "at det er en vellykkede test...";
+    }
+
+}*/
