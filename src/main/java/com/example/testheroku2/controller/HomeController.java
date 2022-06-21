7a717f1409a129ea2eadb434476e333694fa6877
moha <moha729g@stud.kea.dk>
2022-06-21 22:05:21 +0200

    application failed . new controller mapping and reset

---
+++ src/main/java/com/example/testheroku2/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.testheroku2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String getIndex(){
+        return "index5";
+    }
+
+    @GetMapping("/admin-code90")
+    public String getAdminIndex(){
+        return "admin-templates/code-90";
+    }
+}
